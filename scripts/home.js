d7f5be2aed66c4d93ae29025a846f7d69341dd6f
Bodya <bogdan.beryslavskyy@protonmail.com>
2025-10-21 18:28:57 +0400

    new commit

---
+++ scripts/home.js
@@ -0,0 +1,18 @@
+// Инициализация Vanta Net фона
+document.addEventListener("DOMContentLoaded", function() {
+    VANTA.NET({
+      el: "#hero",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 600.00,
+      minWidth: 800.00,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0x00ff99,
+      backgroundColor: 0x0a0a0a,
+      points: 15.00,
+      maxDistance: 20.00,
+      spacing: 18.00
+    });
+  });
+  
